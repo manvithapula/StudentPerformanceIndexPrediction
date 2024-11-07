2617fe865250d373dae329e0c9aeafd25884af57
manvithapula <manvithapula@gmail.com>
2024-11-07 08:12:28 +0530

    Initial Commit

---
+++ StudentPerformance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudentPerformance
+//
+//  Created by admin64 on 07/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StudentPerformanceTests/StudentPerformanceTests.swift
@@ -0,0 +1,17 @@
+//
+//  StudentPerformanceTests.swift
+//  StudentPerformanceTests
+//
+//  Created by admin64 on 07/11/24.
+//
+
+import Testing
+@testable import StudentPerformance
+
+struct StudentPerformanceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
